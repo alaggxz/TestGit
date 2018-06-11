7444d16b6fbde2df04692d871d74dcaff14a8410
eclipse_xuzhen <eclipse_xuzhen@i5cnc.com>
2018-06-11 18:37:54 +0800

    my first commit in eclipse

---
+++ src/main/java/com/i5cnc/git/TestGit.java
@@ -0,0 +1,8 @@
+package com.i5cnc.git;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		System.out.println("测试Git项目开发！！！");
+	}
+}
---
+++ src/test/java/com/i5cnc/test/GitUnitTest.java
@@ -0,0 +1,7 @@
+package com.i5cnc.test;
+
+public class GitUnitTest {
+	public static void main(String[] args) {
+		System.out.println("Gitµ¥Ôª²âÊÔÀà");
+	}
+}
