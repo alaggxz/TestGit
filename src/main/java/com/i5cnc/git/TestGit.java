7a118d31f103103e8cc0bd14ff9be9572126cd3e
eclipse_xuzhen <eclipse_xuzhen@i5cnc.com>
2018-06-12 14:23:12 +0800

    test conflict left

--- src/main/java/com/i5cnc/git/TestGit.java
+++ src/main/java/com/i5cnc/git/TestGit.java
@@ -5,5 +5,6 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("测试Git项目开发！！！");
 		System.out.println("再次提交Git相关文件");
+		System.out.println("left...");
 	}
 }
