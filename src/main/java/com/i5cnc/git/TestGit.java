8eb2d4dc206e1206e90dde4fdf016313c4e4500c
eclipse_xuzhen <eclipse_xuzhen@i5cnc.com>
2018-06-12 14:50:56 +0800

    test hot_fix commit

--- src/main/java/com/i5cnc/git/TestGit.java
+++ src/main/java/com/i5cnc/git/TestGit.java
@@ -5,6 +5,7 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("测试Git项目开发！！！");
 		System.out.println("再次提交Git相关文件");
-		System.out.println("left...");
+		System.out.println("left...right");
+		System.out.println("Bug已修复。。。");
 	}
 }
